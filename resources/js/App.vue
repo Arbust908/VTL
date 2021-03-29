a59269b08b1bfddc20089e407f015db4956d4325
arbust908@gmail.com <arbust908@gmail.com>
2021-03-29 12:35:49 -0300

    App Basics

---
+++ resources/js/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+        <Navbar />
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+import "./Styles/app.sass";
+
+export default {
+    name: "app",
+    components: {
+        Navbar: () => import("./Components/Navbar.vue"),
+    },
+};
+</script>
